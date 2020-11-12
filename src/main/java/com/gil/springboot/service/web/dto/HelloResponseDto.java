72cec36c14428b7680fc465bce9de27634ccca45
GilhoLee <leeeeeegilho@gmail.com>
2020-11-12 11:18:02 +0900

    lombok을 이용한 코드 리펙토링 code refactoring using lombok include TDD

---
+++ src/main/java/com/gil/springboot/service/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.gil.springboot.service.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
