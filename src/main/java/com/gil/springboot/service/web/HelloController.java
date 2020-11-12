096f70f413290a324ffbe10841b9b71c60b1edfb
GilhoLee <leeeeeegilho@gmail.com>
2020-11-12 10:05:59 +0900

    REST Controller 작성 make a Rest Controller class

--- src/main/java/com/gil/springboot/service/web/HelloController.java
+++ src/main/java/com/gil/springboot/service/web/HelloController.java
@@ -1,4 +1,20 @@
 package com.gil.springboot.service.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * return json about request
+ * Before the use this annotation, we did use @ResponseBody that have to write all method.
+ */
+@RestController
 public class HelloController {
+
+    // response about 'GET' request. (@RequestMapping(method=RequestMethod.GET)
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
 }
