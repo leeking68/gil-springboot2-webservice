4b652c89d5a242590d8c540a75873b0225bfd84b
GilhoLee <leeeeeegilho@gmail.com>
2020-11-14 17:00:39 +0900

    make a CRUD class(API)

--- src/main/java/com/gil/springboot/service/posts/PostsService.java
+++ src/main/java/com/gil/springboot/service/posts/PostsService.java
@@ -1,4 +1,19 @@
 package com.gil.springboot.service.posts;
 
+import com.gil.springboot.service.domain.posts.PostRepository;
+import com.gil.springboot.service.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+
+    private  final PostRepository postsRepository;
+
+        @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
--- src/main/java/com/gil/springboot/service/web/dto/PostsSaveRequestDto.java
+++ src/main/java/com/gil/springboot/service/web/dto/PostsSaveRequestDto.java
@@ -1,4 +1,28 @@
 package com.gil.springboot.service.web.dto;
 
+import com.gil.springboot.service.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class PostsSaveRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+
+        return Posts.builder().title(title).content(content).author(author).build();
+
+    }
+
 }
