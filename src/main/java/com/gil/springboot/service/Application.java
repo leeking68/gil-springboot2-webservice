894d3dbdce09a84af8e07ad7098ad3d7101b8165
GilhoLee <leeeeeegilho@gmail.com>
2020-12-02 23:27:26 +0900

    refactoring code

--- src/main/java/com/gil/springboot/service/Application.java
+++ src/main/java/com/gil/springboot/service/Application.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // run JPA Auditing funcㅈㅈ
+@EnableJpaAuditing // run JPA Auditing func
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
     }
 
