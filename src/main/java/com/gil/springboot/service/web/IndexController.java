6b2ee453c051313e9df8991fc375896785603461
GilhoLee <leeeeeegilho@gmail.com>
2020-12-23 02:17:26 +0900

    modify posts update code in IndexController.java

--- src/main/java/com/gil/springboot/service/web/IndexController.java
+++ src/main/java/com/gil/springboot/service/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.gil.springboot.service.web;
 
 import com.gil.springboot.service.posts.PostsService;
+import com.gil.springboot.service.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -17,6 +19,13 @@ public class IndexController {
 
         model.addAttribute("posts",postsService.findAllDesc());
     return "index";
+    }
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 
     @GetMapping("/posts/save")
